fb11775d4fceb0aa22e9c5d2fac2483a76cf4ee2
João Lucas <introducaoteste@gmail.com>
2023-06-24 03:24:24 -0300

    Adicionando módulos no projeto

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 import pygame
-from random import randint
 from pygame.locals import *
-
-
-def random_pos_grid():
-    x = randint(0, WIDTH-10)
-    y = randint(0, HEIGHT-10)
-
-    return (x//20*20, y//20*20)
-
-
-def collision(obj1, obj2):
-    return (obj1[0] == obj2[0]) and (obj1[1] == obj2[1])
+from game.draw import *
+from game.update import *
 
 
 pygame.init()
@@ -20,12 +10,8 @@ SCALE = 20
 SPEED = 20
 WIDTH = 600
 HEIGHT = 600
-UP = 0
-DOWN = 1
-LEFT = 2
-RIGHT = 3
 
-current_direction = DOWN
+current_direction = 1
 
 game_states = {
     'home': 0,
@@ -46,7 +32,11 @@ current_menu = 0
 max_menu_itens = len(game_menu)
 
 canvas = pygame.display.set_mode((WIDTH, HEIGHT))
+icon = pygame.image.load('img/apple_icon.png')
+img_home = pygame.image.load('img/snake_new.png')
+img_home = pygame.transform.smoothscale(img_home, (170, 140))
 pygame.display.set_caption('Snake Game')
+pygame.display.set_icon(icon)
 
 score = 0
 
@@ -58,7 +48,7 @@ snake_pos = [center_canvas, center_canvas, center_canvas, center_canvas]
 snake = pygame.Surface((SCALE-1, SCALE-1))
 snake.fill('green')
 
-apple_pos = random_pos_grid()
+apple_pos = random_pos_grid(canvas)
 apple = pygame.Surface((SCALE, SCALE))
 apple.fill('red')
 
@@ -74,6 +64,7 @@ while not done:
             if game_states['home'] == game_state:
                 if event.key == K_RETURN and game_menu[current_menu][0] == 'play':
                     game_state = 1
+
                 if event.key == K_RETURN and game_menu[current_menu][0] == 'exit':
                     done = True
 
@@ -107,14 +98,7 @@ while not done:
                         SPEED = 20
 
                 if not game_paused:
-                    if event.key in [K_UP, K_w] and current_direction != DOWN:
-                        current_direction = UP
-                    if event.key in [K_DOWN, K_s] and current_direction != UP:
-                        current_direction = DOWN
-                    if event.key in [K_LEFT, K_a] and current_direction != RIGHT:
-                        current_direction = LEFT
-                    if event.key in [K_RIGHT, K_d] and current_direction != LEFT:
-                        current_direction = RIGHT
+                    current_direction = getDirection(event.key, current_direction)
                     
                 if event.key == K_ESCAPE and game_paused:
                     game_state = 0
@@ -122,46 +106,25 @@ while not done:
                     SPEED = 20
 
     if game_states['home'] == game_state:
-        for txt, pos, actived in game_menu:
-            if actived:
-                font = pygame.font.Font('fonts/Roboto-Regular.ttf', 36)
-                font_menu = font.render(f'<< {txt.upper()} >>', True, ('yellow'))
-                font_rect = font_menu.get_rect()
-                font_rect.center = pos
-                canvas.blit(font_menu, font_rect)
-            else:
-                font = pygame.font.Font('fonts/Roboto-Regular.ttf', 36)
-                font_menu = font.render(f'{txt.upper()}', True, ('white'))
-                font_rect = font_menu.get_rect()
-                font_rect.center = pos
-                canvas.blit(font_menu, font_rect)
+        canvas.blit(img_home, (WIDTH//2-90, 20))
+
+        font = pygame.font.Font('fonts/Roboto-Regular.ttf', 36)
+        for item in game_menu:
+            font_menu, font_rect = drawItem(font, item)
+            canvas.blit(font_menu, font_rect)
 
     if game_states['play'] == game_state:
-        # Moviment logic
         if not game_paused:
-            if current_direction == UP:
-                snake_pos[0] = (snake_pos[0][0], snake_pos[0][1]-SPEED)
-            if current_direction == DOWN:
-                snake_pos[0] = (snake_pos[0][0], snake_pos[0][1]+SPEED)
-            if current_direction == LEFT:
-                snake_pos[0] = (snake_pos[0][0]-SPEED, snake_pos[0][1])
-            if current_direction == RIGHT:
-                snake_pos[0] = (snake_pos[0][0]+SPEED, snake_pos[0][1])
+            # Moviment logic
+            moveSnake(snake_pos, current_direction, SPEED)
 
             # Collision logic
-            if snake_pos[0][0] < 0:
-                snake_pos[0] = (WIDTH-SCALE, snake_pos[0][1])
-            if snake_pos[0][0] > WIDTH-SCALE:
-                snake_pos[0] = (0, snake_pos[0][1])
-            if snake_pos[0][1] < 0:
-                snake_pos[0] = (snake_pos[0][0], HEIGHT-SCALE)
-            if snake_pos[0][1] > HEIGHT-SCALE:
-                snake_pos[0] = (snake_pos[0][0], 0)
+            collisionWithCanvas(canvas, snake_pos, SCALE)
 
             if collision(snake_pos[0], apple_pos):
                 snake_pos.append((0, 0))
                 score += 1
-                apple_pos = random_pos_grid()
+                apple_pos = random_pos_grid(canvas)
 
             # Snake body logic
             for i in range(len(snake_pos)-1, 0, -1):
@@ -178,14 +141,12 @@ while not done:
 
     if game_paused:
         font = pygame.font.Font('fonts/Roboto-Regular.ttf', 48)
-        font_pause = font.render('Game Paused', True, ('white'))
-        font_rect = font_pause.get_rect()
-        font_rect.center = center_canvas
+        font_pause, font_rect = drawText(font, 'Game Paused', center_canvas, True)
         canvas.blit(font_pause, font_rect)
 
         font = pygame.font.Font('fonts/Roboto-Regular.ttf', 24)
-        font_back = font.render('[Esc] Back', True, ('white'))
-        canvas.blit(font_back, (20, HEIGHT-40))
+        font_back, font_rect = drawText(font, '[Esc] Back', (20, HEIGHT-40))
+        canvas.blit(font_back, font_rect)
 
     pygame.display.update()
 
